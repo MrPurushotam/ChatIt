ca34fdcb52db485f7e49433aba2dba31984c9cbc
Purushotam <purupurushotamjeswani2004@gmail.com>
2024-11-05 17:17:07 +0530

    Updated server to run over network

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const {createServer}=require("http")
 require('dotenv').config();
 
+const hostip = process.env.HOST_IP || "0.0.0.0";
 const app= require('./index')
 const initalizeSocket= require("./socket/index")
 
@@ -10,6 +11,6 @@ initalizeSocket(server)
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT,()=>{
-    console.log("Server running on 3000")
+server.listen(PORT,hostip,()=>{
+    console.log("Server running on localhost:3000 or "+`${hostip}`)
 })
