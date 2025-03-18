dba54e19be15cdcff8c6d9ef4472d5f700427b39
Purushotam <purupurushotamjeswani2004@gmail.com>
2025-03-19 03:13:02 +0530

    updated notfound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-[60vh] bg-gray-100 text-gray-900 p-6">
+      <div className="flex flex-col items-center justify-center h-full bg-transparent text-gray-900 p-6">
       <h1 className="text-6xl font-bold mb-4">404</h1>
       <p className="text-lg md:text-xl text-gray-600 mb-6">Oops! The page you're looking for doesn't exist.</p>
       <Link
